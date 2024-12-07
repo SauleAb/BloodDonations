392c351b29a199d230a3919d4d80cf923a7b3fd1
SauleAb <abramoviciutesaule@gmail.com>
2024-12-07 11:03:40 +0100

    Refactor Home page

--- app/main/home.tsx
+++ app/main/home.tsx
@@ -1,35 +1,28 @@
-import React from 'react';
-import {StyleSheet, View, ScrollView } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 import CommonBackground from "@/components/common/CommonBackground";
-import CommonContent, {IconNames} from "@/components/common/CommonContent";
-import { getTimeTillNextDonation } from '@/utils/timeUtils';
-import HomeScreenBottom from "@/components/home/HomeScreenBottom"
 import CommonScrollElement from "@/components/common/CommonScrollElement";
+import HomeScreenBottom from "@/components/home/HomeScreenBottom";
+import commonStyles from "../styles/CommonStyles";
+import { homeScreenContent } from "@/constants/HomeData";
+import CommonContent from "@/components/common/CommonContent";
 
 export default function Home() {
-
-    const targetDate = new Date('2024-12-31T23:59:59');
     return (
-        <View style={styles.container}>
+        <View style={commonStyles.container}>
             <CommonBackground logoVisible={true} mainPage={true}>
                 <CommonScrollElement>
-                    <CommonContent titleText={"Notification"} contentText={"awesome content"} icon={IconNames.Notification} />
-                    <CommonContent titleText={"Next Blood Donation"} contentText={getTimeTillNextDonation(targetDate)} icon={IconNames.BloodDonated}/>
-                    <CommonContent titleText={"Blood Donated"} contentText={"1.8L"} icon={IconNames.BloodDrop}/>
-                    <CommonContent titleText={"Next Reward"} contentText={"2 Donations Left"} icon={IconNames.Reward}/>
-                    <CommonContent titleText={"Reward Points"} contentText={"2450"} icon={IconNames.Reward}/>
-                    <CommonContent titleText={"Next Plasma Donation"} contentText={"awesome content"} icon={IconNames.BloodSample}/>
-
-                    <HomeScreenBottom/>
+                    {homeScreenContent.map((item, index) => (
+                        <CommonContent
+                            key={index}
+                            titleText={item.titleText}
+                            contentText={item.contentText}
+                            icon={item.icon}
+                        />
+                    ))}
+                    <HomeScreenBottom />
                 </CommonScrollElement>
             </CommonBackground>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffffff'
-    }
-});
