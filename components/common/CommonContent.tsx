704604d8e1893217f68fa21eddfbfe3b0f09c67b
SauleAb <abramoviciutesaule@gmail.com>
2024-12-07 11:13:02 +0100

    Refactor Rewards page

--- components/common/CommonContent.tsx
+++ components/common/CommonContent.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {useFonts} from "expo-font";
 import CommonText from "@/components/common/CommonText";
 
 export enum IconNames {
