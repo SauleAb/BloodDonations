192beadcdcebab0b739383e88cb3b63b68915c41
Eitve <eitve59@gmail.com>
2024-12-10 09:32:09 +0100

    User Object

---
+++ components/user.tsx
@@ -0,0 +1,34 @@
+const user = {
+    // Basic Info
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    rewardPoints: 0,
+    friendsList: [],
+    posts: [],
+
+    // Plasma Specific Info
+    plasmaDonor: false,
+    nextPlasmaDonation: Date,
+
+    // Blood Specific Info
+    bloodDonor: false,
+    nextBloodDonation: Date,
+    totalBloodDonated: 0,
+
+    // Donation Info
+    donationHistory: [],
+    timesDonated: 0,
+    lastDonation: '',
+
+    // Other
+    ironLevels: 0,
+
+    // Prefferences (this is where all the settings should go)
+    darkModeEnabled: false,
+
+    
+};
+
+export default user;
